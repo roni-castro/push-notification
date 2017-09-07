f889b0ba858da3712ab319637c399a05fdbef903
Roni Castro <roni.castro@ringa.com.br>
2017-09-07 19:33:11 -0300

    Add dynamic link

--- app/src/main/java/android/example/com/firebaseapplication/MainActivity.java
+++ app/src/main/java/android/example/com/firebaseapplication/MainActivity.java
@@ -4,12 +4,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +46,44 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+
+        // [START get_deep_link]
+        FirebaseDynamicLinks.getInstance()
+            .getDynamicLink(getIntent())
+            .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                @Override
+                public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                    // Get deep link from result (may be null if no link is found)
+                    Uri deepLink = null;
+                    if (pendingDynamicLinkData != null) {
+                        deepLink = pendingDynamicLinkData.getLink();
+                    }
+
+
+                    // Handle the deep link. For example, open the linked
+                    // content, or apply promotional credit to the user's
+                    // account.
+                    // ...
+
+                    // [START_EXCLUDE]
+                    // Display deep link in the UI
+                    if (deepLink != null) {
+                        Snackbar.make(findViewById(android.R.id.content),
+                            "Found deep link!", Snackbar.LENGTH_LONG).show();
+                        messageTextView.setText(deepLink.toString());
+                    } else {
+                        Log.d("ERROR", "getDynamicLink: no link found");
+                    }
+                    // [END_EXCLUDE]
+                }
+            })
+            .addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("ERROR", "getDynamicLink:onFailure", e);
+                }
+            });
+        // [END get_deep_link]
     }
 
     private BroadcastReceiver localBroadcastManagerHandler = new BroadcastReceiver(){
